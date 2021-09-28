38b6a853b6f29642375598d0e8103497da9b0a85
Alan T. Arnholt <arnholtat@appstate.edu>
2021-09-28 08:58:17 -0400

    tweakers

---
+++ Rscripts/Dice.R
@@ -0,0 +1,35 @@
+#set.seed(3)
+roll1 <- sample(1:6, 1000000, replace = TRUE)
+roll2 <- sample(1:6, 1000000, replace = TRUE)
+STR <- roll1 + roll2
+A <- STR == 6
+B <- roll1 == 3
+PB <- mean(B)
+PB
+PA <- mean(A)
+PAB <- mean(A & B)
+PAB
+(ans <- PAB/PB)
+
+######
+
+S <- expand.grid(roll1 = 1:6, roll2 = 1:6)
+S$Sum <- apply(S, 1, sum)
+S
+table(S$Sum)
+T1 <- MASS::fractions(table(S$Sum)/dim(S)[1])
+T1
+(x <- as.numeric(names(T1)))
+(px <- T1)
+(EX <- sum(x*px))
+######
+VX <- sum((x - EX)^2*px)
+VX
+
+##############
+
+roll1 <- sample(1:6, 1000000, replace = TRUE)
+roll2 <- sample(1:6, 1000000, replace = TRUE)
+STR <- roll1 + roll2
+mean(STR)
+var(STR)
